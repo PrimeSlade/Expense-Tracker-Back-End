48e3a57470a0a8e1adafa7dc4c1285ea87702774
Slade <saib6064@gmail.com>
2025-03-22 21:34:21 +0700

    feat(controllers): implement subtracting amount from db when create a list

--- controllers/listController/create.js
+++ controllers/listController/create.js
@@ -6,21 +6,42 @@ const create = async(req,res)=>{
 
     const {categories, note, created_at,cost, icon_name} = req.body;
 
-    try {
-        const [data] = await knex('datas').insert({
-            categories: categories,
-            note: note,
-            created_at: created_at,
-            cost: cost,
-            user_id:user_id,
-            icon_name: icon_name
-        }).returning(['id','categories','note','created_at','cost','icon_name']);
-
-        res.json(data);
-    } catch (error) {
-        console.error(error);
-        res.status(400).json({error: "could not create a new list"})
+    //VERY IMPORTANT need to work on this
+    const [{amount}] = await knex.select('amount').where({id:user_id}).from('users');
+    
+     //check whether the amount is greater than or not
+    if(amount < cost){
+        res.status(400).json({error: "insufficient amount"});
+        return;
     }
+
+    knex.transaction(async trx => {
+        try {
+
+            await trx('users').where({id:user_id}).update({
+                amount: amount-cost,
+            })
+
+            const [data] = await trx('datas').insert({
+                categories: categories,
+                note: note,
+                created_at: created_at,
+                cost: cost,
+                user_id:user_id,
+                icon_name: icon_name
+            }).returning(['id','categories','note','created_at','cost','icon_name']);
+    
+            await trx.commit();
+            res.json(data);
+
+        } catch (error) {
+            await trx.rollback();
+            console.error(error);
+            res.status(400).json({error: "could not create a new list"})
+        }
+    })
+
+    
 }
 
 module.exports = create;
