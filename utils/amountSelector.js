cd884b7152de2fbf4b2dd46a57ec165a816eb8b6
Slade <saib6064@gmail.com>
2025-03-24 14:12:22 +0700

    feat(utils): implement error handling

--- utils/amountSelector.js
+++ utils/amountSelector.js
@@ -1,10 +1,17 @@
 const knex = require('../knex/knex.js');
 
 const amountSelector = async(user_id) => {
-    const [{amount}] = await knex.select('amount').where({id:user_id}).from('users');
-    const prevAmount = Number(amount);
-
-    return prevAmount;
+    try {
+        const [{amount}] = await knex.select('amount').where({id:user_id}).from('users');
+        const prevAmount = Number(amount);
+    
+        return prevAmount;
+        
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+   
 }
 
 module.exports = amountSelector;
