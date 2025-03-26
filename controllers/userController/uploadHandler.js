d53a375bed0d38aae86132e74448b64cafde0b7c
Slade <saib6064@gmail.com>
2025-03-26 13:15:14 +0700

    feat(controller): implement inserting url link and img_id  into the db

--- controllers/userController/uploadHandler.js
+++ controllers/userController/uploadHandler.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const cloudinary = require("cloudinary").v2;
 const fs = require("fs");
+const knex = require("../../knex/knex");
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -10,7 +11,19 @@ cloudinary.config({
 
 const uploadHandler = async (req, res) => {
   try {
+    const id = req.user;
     const filePath = req.file.path; // Get the file path from the uploaded file
+
+    //find if the img alr exists
+    const [img] = await knex
+      .select("public_id")
+      .from("users")
+      .where({ id: id });
+
+    if (img.public_id) {
+      await cloudinary.uploader.destroy(img.public_id);
+      console.log("deleted");
+    }
 
     // Upload the file to Cloudinary
     const result = await cloudinary.uploader.upload(filePath, {
@@ -19,6 +32,16 @@ const uploadHandler = async (req, res) => {
 
     // Remove the local image file after upload (it waits the result becoz of the await keyword)
     fs.unlinkSync(filePath);
+
+    //insert
+    await knex("users")
+      .update({
+        img_url: result.secure_url,
+        public_id: result.public_id,
+      })
+      .where({
+        id: id,
+      });
 
     res.status(200).json(result);
   } catch (error) {
