078a137f5676a4afc17a09b70772b6e0443932e4
Slade <saib6064@gmail.com>
2025-04-28 23:53:27 +0700

    feat(util): change accesstoken expire date

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -27,6 +27,7 @@ const requireAuth = async (req, res, next) => {
 
     next();
   } catch (error) {
+    console.log("been there");
     console.log(error);
     res.status(401).json({ error: "Access token is not authorized" });
   }
